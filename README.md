916a5c10c324cde0c704b4adc24e6dcbefcc6c28
William Cao <wcao00@stuy.edu>
2020-02-18 00:24:40 -0500

    added senpai images

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # StuyCSDojo.github.io
 
+## To set up
+1. Put all the senpai photo inside of ./static/photos
+2. Make sure all the photo ends with "\<first> \<last>.jpeg".
+3. Please make sure you remove any exif tags. Use image magick and run ```convert -strip <source> <destination>```
+4. Delete the existing photo code in "about.html". Delete the lines between "<!-- SENPAI IMAGE START --\>" and "<!-- SENPAI IMAGE END --\>". Do NOT delete the comments. 
+5. Run ```$ python3 generateSenpaiImages.py```
+
+* To make the website load faster, you can compress the image more
+
 ## Description
   The Resources page of the CSDojo website is a page for helpful links for Stuyvesant Computer Science classes,
 arranged in an unordered list sorted by coding language in a two-columned table.
---
+++ generateSenpaiImages.py
@@ -0,0 +1,43 @@
+import os
+
+aboutHTML = open("./about.html").read()
+anchor = "<!-- SENPAI IMAGE START -->"
+
+
+def extract_name(filename: str) -> str:
+    """
+    turns "William Cao.jpeg" to "William Cao"
+    """
+    parts = filename[:-5].split(" ")
+    return parts[-2] + " " + parts[-1]
+
+
+# Generate the HTML to be inserted
+photo_names = os.listdir("./static/photos")
+photo_names.sort()
+photo_html = ""
+for file_name in photo_names:
+    if file_name.endswith(".jpeg"):
+        # Ignore directories, extraneous files
+        photo_html += f"""
+        <div class="mdl-shadow--2dp mdl-cell mdl-cell--3-col" style="background: url('./static/photos/{file_name}') center / cover; width: 256px; height: 256px;">
+          <div class="mdl-card__title mdl-card--expand"></div>
+          <div class="mdl-card__actions" style="height: 52px;padding: 16px;background: rgba(255, 255, 255, 0.8);margin-top:150px">
+            <span class="demo-card-image__filename" style="color: #000; font-size: 14px; font-weight: 500;">{extract_name(file_name)}</span>
+          </div>
+        </div>
+        """
+
+
+# Wrap in a grid div
+photo_html = f"""
+<div class="mdl-grid">
+{photo_html}
+</div>
+"""
+# Insert the html
+index_to_insert = aboutHTML.index(anchor) + len(anchor) + 2
+aboutHTML = aboutHTML[:index_to_insert] + photo_html + aboutHTML[index_to_insert:]
+
+print(aboutHTML)
+open('about.html', 'w').write(aboutHTML)
